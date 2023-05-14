837afa4e5edd62197d04da5245012fd074637b20
salmalogics <101881502+salmalogics@users.noreply.github.com>
2023-05-15 02:20:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Router from './Router';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Router/>
+    </div>
+  );
+}
+
+export default App;
